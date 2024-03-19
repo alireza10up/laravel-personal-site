3f19ead03ffe8fd66102f57b2392105cac252a07
alireza10up <toolsalireza10up@gmail.com>
2024-03-20 03:19:12 +0330

    define rotes and views

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,26 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // TODO Complete it
+    return view('home');
+});
+
+Route::get('/resume', function () {
+    // TODO Complete it
+   return view('coming-soon');
+});
+
+Route::get('/portfolio', function () {
+    // TODO Complete it
+    return view('coming-soon');
+});
+
+Route::get('/contact', function () {
+    // TODO Complete it
+    return view('coming-soon');
+});
+
+Route::get('/blog', function () {
+    // TODO Complete it
+    return view('coming-soon');
 });
