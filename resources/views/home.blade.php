4030400d50c85b42edf6171e3e29e70d72bf35a2
alireza10up <toolsalireza10up@gmail.com>
2024-07-15 20:31:39 +0330

    fix home problem

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,43 +17,46 @@
         </div>
 
         <!-- What -->
-        <div class="section">
-            <h2 class="title title--h2">توی این مسائل کمکت می کنم</h2>
-            <div class="row">
-                @forelse($settings->what_i_do ?? [] as $iDoName => $iDoInformation)
-                    <!-- Case Item -->
-                    <div class="col-12 col-lg-6 case-item-wrap">
-                        <div class="case-item">
-                            <img class="case-item__icon" src="{{ asset($iDoInformation['icon'] ?? '') }}" alt="{{ $iDoName ?? '' }}"/>
-                            <h3 class="title title--h3">{{ $iDoName ?? '' }}</h3>
-                            <p class="case-item__caption">{{ $iDoInformation['description'] ?? '' }}</p>
+        @empty(!($settings->what_i_do ?? ''))
+            <div class="section">
+                <h2 class="title title--h2">توی این مسائل کمکت می کنم</h2>
+                <div class="row">
+                    @forelse($settings->what_i_do ?? [] as $iDoInformation)
+                        <!-- Case Item -->
+                        <div class="col-12 col-lg-6 case-item-wrap">
+                            <div class="case-item">
+                                <img class="case-item__icon" src="{{ Storage::url($iDoInformation['icon'] ?? '') }}"
+                                     alt="{{ $iDoInformation['title'] ?? '' }}"/>
+                                <h3 class="title title--h3">{{ $iDoInformation['title'] ?? '' }}</h3>
+                                <p class="case-item__caption">{{ $iDoInformation['description'] ?? '' }}</p>
+                            </div>
                         </div>
-                    </div>
-                @empty
-                    <h4>چیزی یافت نشد</h4>
-                @endforelse
+                    @empty
+                        <h6>چیزی یافت نشد</h6>
+                    @endforelse
+                </div>
             </div>
-        </div>
+        @endempty
 
         <!-- Testimonials -->
-        @if($settings->feedbacks_enabled)
+        @if($settings->feedbacks_enabled ?? false)
             <div class="section">
                 <h2 class="title title--h2">نظرات مشتریان</h2>
                 <div class="swiper-container js-carousel-review">
                     <div class="swiper-wrapper">
-                        @forelse($feedbacks as $feedback)
+                        @forelse($feedbacks ?? [] as $feedback)
                             <!-- Item review -->
                             <div class="swiper-slide review-item">
                                 <svg class="avatar avatar--80" viewBox="0 0 84 84">
                                     <g class="avatar__hexagon">
-                                        <image xlink:href="{{ asset('storage/' . $feedback->profile) }}" height="100%" width="100%"/>
+                                        <image xlink:href="{{ Storage::url('storage/' . $feedback->profile ?? '') }}" height="100%" width="100%"/>
                                     </g>
                                 </svg>
                                 <h4 class="title title--h3">{{ $feedback->name ?? '' }}</h4>
                                 <p class="review-item__caption">{{ $feedback->description ?? '' }}</p>
                             </div>
                         @empty
-                            <h4>چیزی یافت نشد</h4>
+                            <h6>چیزی یافت نشد</h6>
                         @endforelse
                     </div>
                     <div class="swiper-pagination"></div>
@@ -62,20 +65,20 @@
         @endif
 
         <!-- Clients -->
-        @if($settings->customers_enabled)
+        @if($settings->customers_enabled ?? false)
             <div class="section">
                 <h2 class="title title--h2">مشتریان</h2>
                 <div class="swiper-container js-carousel-clients">
                     <div class="swiper-wrapper">
-                        @forelse($customers as $customer)
+                        @forelse($customers ?? [] as $customer)
                             <!-- Item client -->
                             <div class="swiper-slide">
-                                <a href="{{ $customer->link }}">
-                                    <img src="{{ asset('storage/' . $customer->logo) }}" alt="{{ $customer->name }}"/>
+                                <a href="{{ $customer->link ?? '' }}">
+                                    <img src="{{ asset('storage/' . $customer->logo ?? '') }}" alt="{{ $customer->name ?? '' }}"/>
                                 </a>
                             </div>
                         @empty
-                            <h4>چیزی یافت نشد</h4>
+                            <h6>چیزی یافت نشد</h6>
                         @endforelse
                     </div>
                     <div class="swiper-pagination"></div>
